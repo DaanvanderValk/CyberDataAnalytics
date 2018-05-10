9d26d1ab2a116f3d4613d284b1a4f6b70381890d
SandeshMj <sandesh.mj2805@gmail.com>
2018-05-10 17:36:05 +0200

    Decision tree on our data and decision tree example

---
+++ Assignment 1/Code/White-Box/DecisionTreeExample.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  6 14:30:34 2018
+
+@author: sande
+"""
+
+from sklearn.datasets import load_iris
+from sklearn import tree
+import graphviz
+from IPython.display import Image  
+import pydotplus
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris.data, iris.target)
+X = iris.data
+y = iris.target
+dot_data = tree.export_graphviz(clf, out_file=None,class_names=["clas1","class2","class3"], node_ids=True) 
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+path = clf.decision_path(X_test)
+print(type(path))
+print(path)
+graph = graphviz.Source(dot_data) 
+graph.render("iris") 
+print ("Zeroth sample",X_test[0])
+pred = clf.predict(X_test)
+print (pred[0])
