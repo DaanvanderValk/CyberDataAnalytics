c4e3fdfabed07cf55ed176db6bac2a5eca7e5f09
SandeshMj <sandesh.mj2805@gmail.com>
2018-05-11 18:00:22 +0200

    Changes in decision tree to include path

--- Assignment 1/Code/White-Box/DecisionTree.py
+++ Assignment 1/Code/White-Box/DecisionTree.py
@@ -13,7 +13,7 @@ get_ipython().magic(u'matplotlib inline')
 import datetime
 import time
 import matplotlib.pyplot as plt
-import joblib
+#import joblib
 from sklearn import neighbors
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import f1_score
@@ -30,6 +30,9 @@ from sklearn.metrics import recall_score
 from imblearn.over_sampling import SMOTE 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_curve, auc
+from sklearn import tree
+import graphviz
+import scipy.sparse
 
 def string_to_timestamp(date_string):#convert time string to float value
     time_stamp = time.strptime(date_string, '%Y-%m-%d %H:%M:%S')
@@ -136,24 +139,113 @@ x_train, x_test, y_train, y_test = train_test_split(x_array, y_array, test_size 
 
 
 print("decision tree Classifier")
-clf = tree.DecisionTreeClassifier(max_depth=6)
+clf = tree.DecisionTreeClassifier(max_depth=10)
 clf.fit(x_train, y_train)
+
+y_pred_smote_randomforest = clf.predict_proba(x_test)[:,1]
+y_pred_smote_randomforest_bin = np.around(y_pred_smote_randomforest)
+print("recall score:", recall_score(y_test, y_pred_smote_randomforest_bin))
+print("precision score:", precision_score(y_test, y_pred_smote_randomforest_bin))
+print("f1 score:", f1_score(y_test, y_pred_smote_randomforest_bin))            
+
 dot_data = tree.export_graphviz(clf, out_file=None,class_names=["non-fraud","fraud"],filled=True,impurity=True,feature_names=["issuercountry", "txvariantcode", "issuer_id", "amount_in_eur", "currencycode",
                     "shoppercountry", "interaction", "verification", "cvcresponse", "creationdate_stamp",
                      "accountcode", "mail_id", "ip_id", "card_id"], node_ids=True) 
 graph = graphviz.Source(dot_data) 
-#
-graph.render("iriss") 
+# Save the decision tree in a pdf file names cardtree
+graph.render("cardtree") 
 
-print ("Zeroth sample",x_test[0])
 
 path = clf.decision_path(x_test)
-print(type(path))
-print(path)
+#print(type(path))
+#print(path)
 pred = clf.predict(x_test)
-for i in range(0,len(x_test)):
-    if (pred[i] != y_test[i]):
-        print ("Wrong prediction for entry ", i)
-        print (pred[i],y_test[i])
-        print ("for row ",x_test[i])
-        break
+#Flags to stop after finding one true positive and one true negative
+flag1 = False
+flag2 = False
+index_of_TN = 0
+index_of_TP = 0
+for i in range(1000,len(x_test)):
+    if flag1 == False :
+        if (pred[i] == y_test[i] == 0):
+            print ("True Negative at index ", i)
+            index_of_TN = i
+            print (pred[i],y_test[i])
+            print ("for row ",x_test[i])
+            flag1 = True
+    if flag2 == False :
+        if (pred[i] == y_test[i] == 1):
+            print ("True positive at index ", i)
+            print (pred[i],y_test[i])
+            print ("for row ",x_test[i])
+            index_of_TP = i
+            if(flag1 == True):
+                break
+        
+
+cx = scipy.sparse.coo_matrix(path)
+
+#find relevant entries in the decision tree for True negative case and true positive case
+for i,j,v in zip(cx.row, cx.col, cx.data):
+    if i == index_of_TN :
+        print ("Nodes for True Negative")
+        print ("(%d, %d), %s" % (i,j,v))
+    if i == index_of_TP:
+        print ("Nodes for True positive")
+        print ("(%d, %d), %s" % (i,j,v))
+        
+'''
+#for i in range(0,len(x_test)):
+#    if (pred[i] == 1):
+#        print ("positive at index ", i)
+#        print (pred[i],y_test[i])
+#find the best SMOTE ratio
+#i = 0.05               
+#while (i<=1.0):
+#    sm = SMOTE(ratio=i)
+#    
+#    x_train_smote = x_train.astype(float)
+#    y_train_smote = y_train.astype(float)
+#    x_train_res, y_train_res = sm.fit_sample(x_train_smote, y_train_smote) 
+#    print ("for ratio ",i)
+#    print("SMOTEd training set:", sum(y_train_res), "/", len(y_train_res), "records are fraud ==> smote_ratio =", i)
+#    clf.fit(x_train_res, y_train_res)
+#    
+#    #print ("after SMOTE")
+#    y_pred_smote_randomforest = clf.predict_proba(x_test)[:,1]
+#    y_pred_smote_randomforest_bin = np.around(y_pred_smote_randomforest)
+#    #print("recall score:", recall_score(y_test, y_pred_smote_randomforest_bin))
+#    #print("precision score:", precision_score(y_test, y_pred_smote_randomforest_bin))
+#    print("f1 score:", f1_score(y_test, y_pred_smote_randomforest_bin))
+#    i = i+0.05
+
+##Among all the SMOTE ratios, a ratio of 0.05 had the best f1 score
+#sm = SMOTE(ratio=0.05)
+#    
+#x_train_smote = x_train.astype(float)
+#y_train_smote = y_train.astype(float)
+#x_train_res, y_train_res = sm.fit_sample(x_train_smote, y_train_smote) 
+#print ("for ratio ",i)
+#print("SMOTEd training set:", sum(y_train_res), "/", len(y_train_res), "records are fraud ==> smote_ratio =", i)
+#clf.fit(x_train_res, y_train_res)
+#    
+#print ("after SMOTE")
+#y_pred_smote_randomforest = clf.predict_proba(x_test)[:,1]
+#y_pred_smote_randomforest_bin = np.around(y_pred_smote_randomforest)
+#print("recall score:", recall_score(y_test, y_pred_smote_randomforest_bin))
+#print("precision score:", precision_score(y_test, y_pred_smote_randomforest_bin))
+#print("f1 score:", f1_score(y_test, y_pred_smote_randomforest_bin))
+        
+#try for difference combinationsfor max depth and min sample split and obtain the better performing ones
+#for i in [2,3,4,5,6,7,8,9]:
+#    #for j in [6,9,12,15,21,27,30,33,37]:
+#    for j in [30,33,37]:
+#        print("decision tree Classifier for ",i,j)
+#        clf = tree.DecisionTreeClassifier(min_samples_split = i,max_depth=j, random_state = 12)
+#        clf.fit(x_train, y_train)
+#        y_pred_smote_randomforest = clf.predict_proba(x_test)[:,1]
+#        y_pred_smote_randomforest_bin = np.around(y_pred_smote_randomforest)
+#        print("f1 score:", f1_score(y_test, y_pred_smote_randomforest_bin))
+#    
+
+'''
